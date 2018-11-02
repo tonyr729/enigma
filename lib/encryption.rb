b2f24d6037935efae2c9e7ed1f3d092b16dc0ce0
Tony Robertson <tonyr729@gmail.com>
2018-11-02 11:10:35 -0600

    Add impementation of coverting key and date to rotational cypher

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -6,10 +6,17 @@ class Encryption
   def encrypt_message(message, key, date)
     date_key = get_date_key(date)
     rotation_cypher = convert_key_and_date(key, date_key)
+
+  end
+
+  def convert_key_and_date(key, date_key)
+    key_cycle = key.chars.each_cons(2)
+    rotation = date_key.chars.map {|num| key_cycle.next.join.to_i + num.to_i}
+    rotation.cycle
   end
 
   def get_date_key(date)
     date = date.sub!(/^0/, "") if date[0] == "0"
-    x = (date.to_i ** 2).to_s[-4..-1].to_i
+    (date.to_i ** 2).to_s[-4..-1]
   end
 end
