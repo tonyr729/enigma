cecde1d7ad1a0725d0cc64c08a337923f1a6ab5e
Tony Robertson <tonyr729@gmail.com>
2018-11-02 10:38:47 -0600

    WIP: Add encrypt method requires get_date_key method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,11 @@
 class Encryption
+  def initialize
+    @char_set = ("a".."z").to_a << " "
+  end
+
+  def encrypt_message(message, key, date)
+    date_key = get_date_key(date)
+    rotation_cypher = convert_key_and_date(key, date_key)
+    require "pry"; binding.pry
+  end
 end
