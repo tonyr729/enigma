fa6c7e6dd86c506e3b5205073fbe292ca335e865
Tony Robertson <tonyr729@gmail.com>
2018-11-02 10:08:22 -0600

    Add encrypt and decrypt classes to engima initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,9 @@
 class Enigma
 
-  def initialize(argument)
-    @argument = argument
+  attr_reader :encryption, :decryption
+
+  def initialize
+    @encryption = Encryption.new
+    @decryption = Decryption.new
   end
 end
