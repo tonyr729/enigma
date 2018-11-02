8d13ffb829b3024a8394a9a8cb238a9e5ef54d13
Tony Robertson <tonyr729@gmail.com>
2018-11-02 10:54:51 -0600

    Add implementation of get_date_key helper method test

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -6,6 +6,10 @@ class Encryption
   def encrypt_message(message, key, date)
     date_key = get_date_key(date)
     rotation_cypher = convert_key_and_date(key, date_key)
-    require "pry"; binding.pry
+  end
+
+  def get_date_key(date)
+    date = date.sub!(/^0/, "") if date[0] == "0"
+    x = (date.to_i ** 2).to_s[-4..-1].to_i
   end
 end
