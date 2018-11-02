2c14883c269022bde0a803552bad11074243a573
Tony Robertson <tonyr729@gmail.com>
2018-11-02 10:10:52 -0600

    Add test for encrypt_message method

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -8,4 +8,17 @@ class EncryptionTest < Minitest::Test
   def test_it_exists
     assert_instance_of Encryption, @encryption
   end
+
+  def test_it_can_encrypt_message
+    skip
+    message = "hello world end"
+    key = "12345"
+    date = Time.now.strftime("%Y%m%d")
+
+    actual = @encryption.encrypt_message(message, key, date)
+
+    assert_instance_of String, actual
+    assert_equal message.length, actual.length
+    refute message = actual
+  end
 end
