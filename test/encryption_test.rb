733eb3e90095f45f7785783c778cc8b9b7272871
Tony Robertson <tonyr729@gmail.com>
2018-11-02 11:09:55 -0600

    Change expected output datatypes

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -23,19 +23,18 @@ class EncryptionTest < Minitest::Test
   end
 
   def test_converts_key_and_date_to_rotation_cypher
-    skip
     key = "12345"
     date_key = "1102"
     actual = @encryption.convert_key_and_date(key, date_key)
 
-    assert_instance_of Enumerable, actual
-    assert_equal [12], actual.next
+    assert_instance_of Enumerator, actual
+    assert_equal 13, actual.next
   end
 
   def test_it_gets_date_key
     date = "021118"
     actual = @encryption.get_date_key(date)
 
-    assert_equal 9924, actual
+    assert_equal "9924", actual
   end
 end
