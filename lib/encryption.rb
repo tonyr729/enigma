a2365d54613b8788455690b77e247e3ab57017bc
Tony Robertson <tonyr729@gmail.com>
2018-11-02 12:19:21 -0600

    Add implementation for cyphering message

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -6,7 +6,12 @@ class Encryption
   def encrypt_message(message, key, date)
     date_key = get_date_key(date)
     rotation_cypher = convert_key_and_date(key, date_key)
-
+    set = @char_set.join
+    encrypted = message.chars.map do |char|
+      cypher_set = @char_set.rotate(rotation_cypher.next).join
+      char.tr(set, cypher_set)
+    end
+    encrypted.join
   end
 
   def convert_key_and_date(key, date_key)
