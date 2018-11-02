76bc507c21c2ce3d33c7f40fe2a3e0f2c06ac266
Tony Robertson <tonyr729@gmail.com>
2018-11-02 10:00:19 -0600

    Add test to assert Engima has instance of Encryption

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,12 +1,16 @@
 require 'test_helper.rb'
 
+
 class EnigmaTest < Minitest::Test
   def setup
-    @enigma = Enigma.new("")
+    @enigma = Enigma.new
   end
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_has_encryption
+    assert_instance_of Encryption, @enigma.encryption
+  end
 end
