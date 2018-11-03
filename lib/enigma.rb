aebfed1430a6337d62bde21e24709a18ca443fd9
Tony Robertson <tonyr729@gmail.com>
2018-11-03 10:24:48 -0600

    Add cypher to initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,11 @@
 class Enigma
 
-  attr_reader :encryption, :decryption
+  attr_reader :encryption, :decryption, :cypher
 
   def initialize
     @encryption = Encryption.new
     @decryption = Decryption.new
+    @cypher = Cypher.new
   end
 
   def encrypt(message, key, date)
