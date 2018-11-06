150a5df8631c0cdb8d8779168846491b812f11c1
Tony Robertson <tonyr729@gmail.com>
2018-11-06 14:42:20 -0700

    Change method in test_helper to ingore runner files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,8 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'mocha/minitest'
-Dir["./lib/*.rb"].each {|file| require file }
+Dir["./lib/*.rb"].each do |file|
+  if file != "./lib/encrypt.rb" && file != "./lib/decrypt.rb"
+    require file
+  end
+end
