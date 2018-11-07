80f4cd3869a40b15f9c44a29c781bda16e1cf463
Tony Robertson <tonyr729@gmail.com>
2018-11-07 10:52:38 -0700

    Add test for generating key cycle

--- test/cypher_test.rb
+++ test/cypher_test.rb
@@ -9,7 +9,7 @@ class CypherTest < Minitest::Test
     assert_instance_of Cypher, @cypher
   end
 
-  def test_converts_key_and_date_to_rotation_cypher
+  def test_generates_a_rotation_cypher
     key = "12345"
     key_cycle = key.chars.each_cons(2)
     date_key = "1102"
@@ -18,6 +18,31 @@ class CypherTest < Minitest::Test
 
     assert_instance_of Enumerator, actual
     assert_equal 13, actual.next
+  end
+
+  def test_it_generates_key_cycle
+    actual = @cypher.generate_key_cycle("12345")
+
+    assert_instance_of Enumerator, actual
+    assert_equal ["1", "2"], actual.next
+    assert_equal [["1", "2"], ["2", "3"], ["3", "4"], ["4", "5"]], actual.to_a
+  end
+
+  def test_it_gets_date_key
+    date = "021118"
+    actual = @cypher.get_date_key(date)
+
+    assert_equal "9924", actual
+  end
+
+  def test_it_can_generate_a_key
+    actual = @cypher.key_gen
+
+    assert_instance_of String, actual
+    assert_equal 5, actual.length
+    actual.chars.each do |char|
+      assert_instance_of Integer, char.to_i
+    end
   end
 
   def test_it_can_generate_a_date
@@ -31,22 +56,5 @@ class CypherTest < Minitest::Test
     actual.chars.each do |char|
       assert_instance_of Integer, char.to_i
     end
-  end
-
-  def test_it_can_generate_a_key
-    actual = @cypher.key_gen
-
-    assert_instance_of String, actual
-    assert_equal 5, actual.length
-    actual.chars.each do |char|
-      assert_instance_of Integer, char.to_i
-    end
-  end
-
-  def test_it_gets_date_key
-    date = "021118"
-    actual = @cypher.get_date_key(date)
-
-    assert_equal "9924", actual
   end
 end
